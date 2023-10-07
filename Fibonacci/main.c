2f6d8c02deb754faf1b0851caee5dc8c0294bc6b
Alessandro Monte <46656617+Mongisa@users.noreply.github.com>
2023-10-07 10:12:28 +0000

    Fibonacci completato

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -13,11 +13,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+unsigned long int fibonacci(int n);
+
 int main(int argc, char** argv) {
+
+    int n;
+    unsigned long int f;
+
+    printf("Inserisci n: ");
+    scanf("%d",&n);
+
+    f = fibonacci(n);
+
+    printf("Il numero di Fibonacci di n=%d è: %ld",n,f);
 
     return (EXIT_SUCCESS);
 }
 
+unsigned long int fibonacci (int n) {
+    if(n==0) {
+        return 0;
+    } else if (n==1) {
+        return 1;
+    }
+
+    return fibonacci(n-1)+fibonacci(n-2);
+}
