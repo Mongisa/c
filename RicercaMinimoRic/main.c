f554de3690c7874d884097161814477afa04daed
Alessandro Monte <46656617+Mongisa@users.noreply.github.com>
2023-10-11 18:13:27 +0000

    Risolto RicercaMinimoRic

--- RicercaMinimoRic/main.c
+++ RicercaMinimoRic/main.c
@@ -12,12 +12,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#define DIM 7
 
-/*
- * 
- */
+typedef int infoTy;
+
+int ricercaMinimoRic(infoTy vet[], int n);
+bool less(infoTy a, infoTy b) {
+    if(a < b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int main(int argc, char** argv) {
+
+    infoTy vet[DIM] = {2,3,-50,1,0,3,-20};
+
+    printf("Il minimo valore ha indice %d\n",ricercaMinimoRic(vet, DIM));
 
     return (EXIT_SUCCESS);
 }
 
+int ricercaMinimoRic(infoTy vet[], int n){ 
+
+    if(n==1) return 0; //CASO BASE
+
+    int min_I = ricercaMinimoRic(vet,n-1); //DIVIDE ET IMPERA
+    
+    if(less(vet[min_I],vet[n-1])) { //COMBINA
+        return min_I;
+    } else {
+        return n-1;
+    }
+
+};
+
+/*int ricercaMinimoRic(infoTy vet[], int n){ //VERSIONE RIDOTTA
+
+    if(n==1) return 0;
+
+    int min_I = ricercaMinimoRic(vet,n-1);
+
+    return (less(vet[min_I],vet[n-1])) ? min_I : n-1;
+
+};*/
